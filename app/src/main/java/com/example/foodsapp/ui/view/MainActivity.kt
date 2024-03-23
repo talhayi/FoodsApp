7f5abdfa02d1105e2487f9ba9daea9f885c50303
talhayi <talhayldrm23@gmail.com>
2024-03-23 06:21:09 +0300

    Added global action for CartFragment

--- app/src/main/java/com/example/foodsapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/foodsapp/ui/view/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.foodsapp.ui.view
 
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.foodsapp.MainNavigationDirections
 import com.example.foodsapp.R
 import com.example.foodsapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,14 +14,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       /* requestWindowFeature(Window.FEATURE_NO_TITLE)
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        supportActionBar?.hide()*/
-        //WindowCompat.setDecorFitsSystemWindows(window,false)
+        supportActionBar?.hide()
+        WindowCompat.setDecorFitsSystemWindows(window,false)*/
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bottomNavigationView.background = null
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
+        binding.cartFAB.setOnClickListener {
+            navHostFragment.navController.navigate(MainNavigationDirections.actionGlobalToCartFragment())
+        }
     }
 }
