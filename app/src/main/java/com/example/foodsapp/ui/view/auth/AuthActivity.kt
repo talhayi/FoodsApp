bb638ec0a7d53c675016bb16f78faf5faaf90e81
talhayi <talhayldrm23@gmail.com>
2024-03-26 07:09:04 +0300

    Added splash screen

--- app/src/main/java/com/example/foodsapp/ui/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/foodsapp/ui/view/auth/AuthActivity.kt
@@ -1,8 +1,12 @@
 package com.example.foodsapp.ui.view.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.foodsapp.databinding.ActivityAuthBinding
+import com.example.foodsapp.ui.view.food.MainActivity
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
@@ -10,5 +14,11 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@AuthActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
     }
 }
